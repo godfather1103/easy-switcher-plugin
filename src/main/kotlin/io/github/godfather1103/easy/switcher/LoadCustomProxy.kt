d85c155ac46f3fe6acee9f62907c7e9d9303d89a
Jack Chu <chuchuanbao@gmail.com>
2025-07-24 20:15:59 +0800

    feat: 启动时加载自定义代理
    - 实现了 AppLifecycleListener 接口，监听应用生命周期事件
    - 在 appFrameCreated 方法中添加了代理加载逻辑- 恢复了 plugin.xml 中的 applicationListeners 配置

--- src/main/kotlin/io/github/godfather1103/easy/switcher/LoadCustomProxy.kt
+++ src/main/kotlin/io/github/godfather1103/easy/switcher/LoadCustomProxy.kt
@@ -1,12 +1,16 @@
 package io.github.godfather1103.easy.switcher
 
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import io.github.godfather1103.easy.switcher.settings.AppSettings
 
-internal class LoadCustomProxy : ProjectActivity {
+internal class LoadCustomProxy : ProjectActivity, AppLifecycleListener {
     override suspend fun execute(project: Project) {
         CustomProxy.reset(AppSettings.instance.state)
     }
 
+    override fun appFrameCreated(commandLineArgs: List<String?>) {
+        CustomProxy.reset(AppSettings.instance.state)
+    }
 }
