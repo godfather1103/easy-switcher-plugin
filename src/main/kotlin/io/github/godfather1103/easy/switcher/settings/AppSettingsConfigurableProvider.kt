ae7294ac80d3c105621d7697f5194ce1e796372c
Jack Chu <chuchuanbao@gmail.com>
2025-07-24 12:22:02 +0800

    refactor(settings): 将 AppSettingsConfigurableProvider 从 Java 迁移到 Kotlin
    - 删除了 Java版本的 AppSettingsConfigurableProvider 类
    - 新增了 Kotlin版本的 AppSettingsConfigurableProvider 类
    - 保留了原有的功能和逻辑

---
+++ src/main/kotlin/io/github/godfather1103/easy/switcher/settings/AppSettingsConfigurableProvider.kt
@@ -0,0 +1,20 @@
+package io.github.godfather1103.easy.switcher.settings
+
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.options.ConfigurableProvider
+import io.github.godfather1103.easy.switcher.ui.Settings
+
+/**
+ * <p>Title:        Godfather1103's Github</p>
+ * <p>Copyright:    Copyright (c) 2025</p>
+ * <p>Company:      <a href="https://github.com/godfather1103">https://github.com/godfather1103</a></p>
+ * 类描述：
+ *
+ * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+ * @version 1.0
+ * @date 创建时间：2025/7/22 12:03
+ * @since 1.0
+ */
+class AppSettingsConfigurableProvider : ConfigurableProvider() {
+    override fun createConfigurable(): Configurable? = Settings()
+}
