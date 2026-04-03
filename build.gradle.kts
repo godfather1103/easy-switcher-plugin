27e072e07501c6cd163e7de648ed7a0623ebcb55
Jack Chu <chuchuanbao@gmail.com>
2026-04-03 16:44:10 +0800

    chore(deps): 更新 Kotlin 和 IntelliJ 平台插件版本
    - 将 org.jetbrains.kotlin.jvm 插件从 2.1.20 更新到 2.3.20
    - 将 org.jetbrains.intellij.platform 插件从 2.5.0 更新到 2.11.0
    - 将 IntelliJ IDEA 版本从 2024.3 更新到 2026.1
    - 更新 sinceBuild 版本号从 242 到 261
    - 在 CustomProxy 中添加 RequestorType 导入
    - 修改 requestPasswordAuthenticationInstance 方法调用参数结构
    - 添加空字符串和 RequestorType.SERVER 参数到认证方法调用

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.20"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.kotlin.jvm") version "2.3.20"
+    id("org.jetbrains.intellij.platform") version "2.11.0"
 }
 
 group = "io.github.godfather1103"
@@ -25,7 +25,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:${property("lombok.version")}")
     annotationProcessor("org.projectlombok:lombok:${property("lombok.version")}")
     intellijPlatform {
-        create("IC", "2024.3")
+        intellijIdea("2026.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add necessary plugin dependencies for compilation here, example:
@@ -54,7 +54,7 @@ intellijPlatform {
     }
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "242"
+            sinceBuild = "261"
         }
         changeNotes = """
       优化测试描述说明
